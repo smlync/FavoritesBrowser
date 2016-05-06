37d9cec1e563e7d7a47da1b8c583ad1fa4d5e660
mmutz <mmutz@DESKTOP-LDJ9D93>
2016-05-06 15:55:39 -0500

    Add project files.

---
+++ FavoritesBrowser/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace FavoritesBrowser
+{
+	public class Category
+	{
+		public int CategoryID;
+		public string CategoryName;
+
+		public List<Product> CategoryProducts { private set; get; }
+
+		public Category(int id, string name)
+		{
+			CategoryID = id;
+			CategoryName = name;
+		}
+	}
+}
---
+++ FavoritesBrowser/Views/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FavoritesBrowser
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
